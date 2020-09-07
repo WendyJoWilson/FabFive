165088397c4fcdd893f848c684c52ce198a2db9a
WendyJoWilson <wendyjoannwilson@gmail.com>
2020-09-07 10:55:21 -0500

    Create PRODUCT.cbl
    Creating place holder for Final Code.

---
+++ PROD/PRODUCT.cbl
@@ -0,0 +1,20 @@
+      IDENTIFICATION DIVISION.
+       PROGRAM-ID. PRODUCT.
+      
+      ***************************************************************** 
+      *              FINAL EXAM:  FABFIVE TEAM PROJECT
+      * 
+      *     ENTERPRISE COBOL FOR BUSINESS APPLICATION DEVELOPMENT 
+      *      COURSE PROVIDED BY THE IBM CORPORATION SUMMER 2020 
+      *             CLASS INSTRUCTOR:  JONATHAN SAYLES 
+      ***************************************************************** 
+      *****************************************************************
+      *                      THE FAB FIVE TEAM   
+      *                  TECH-LEAD:  RICHARD DARROW
+      *           PROGRAMMER/ANALYST:  AGAPIOS KALOKERINOS
+      *             PROGRAMMER/ANALYST:  DORETHA RILEY
+      *              TESTER:  ROBERTA ADRIANA de SOUZA
+      *                 TESTER:  WENDY J. WILSON
+      ***************************************************************** 
+      *****************************************************************
+      
